73590674ccb299551b6374354b6cf2cde8da2724
Yarden Katz <kyarden@cambium.co.il>
2020-08-03 16:56:15 +0300

    parsing json from serer

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,52 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by David Salzer on 08/07/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=b4fa09a9481ea9770269a249c8053261&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create a URL
+        if let url = URL(string: urlString) {
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+        
+    }
+}
